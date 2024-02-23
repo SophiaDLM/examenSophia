112461f33a38dfdb658d62763cb82d4bdee9147d
SophiaDLM <SophiaDLM@outlook.es>
2024-02-23 19:14:00 +0100

    Añadidos comentarios de documentación en claseExamen

--- src/miExamen/claseExamen.java
+++ src/miExamen/claseExamen.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package miExamen;
 
 /**
- *
- * @author Sophi
+ * <h1>Entornos de desarrollo UT3</h1>
+ * <p>Esta clase sirve para realizar el examen de la UT3 del módulo Entornos de Desarrollo</p><br>
+ * <a href="https://www.youtube.com/playlist?list=PLU8oAlHdN5BlyaPFiNQcV0xDqy0eR35aU" target="_blank">Curso de Git y github de píldoras informáticas</a>
+ * 
+ * @author Sophia De Luca Miranda
+ * @version 1.0.0
+ * @since 23/02/2024
  */
 public class claseExamen {
     
+    public void main(String[] args) {
+        System.out.println("Estoy haciendo el examen de la UT3");
+    }
 }
